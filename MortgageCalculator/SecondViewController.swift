76461328fcf997fe527e7a53c67efb777de1d9fe
Gaurav Chodwadia <gauravchodwadia@gmail.com>
2017-04-28 23:22:29 -0700

    Initial Commit

---
+++ MortgageCalculator/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  MortgageCalculator
+//
+//  Created by Gaurav Chodwadia on 4/28/17.
+//  Copyright © 2017 Gaurav Chodwadia. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
